075779f500cffbdf6bba6c9c23661d4ceb2e9acb
Aleksey Petrik <alexey.petric@gmail.com>
2022-04-19 21:00:48 +1000

    first commit, begin telegram bot

---
+++ src/main/java/com/bot/app/appconfig/BotConfig.java
@@ -0,0 +1,37 @@
+package com.bot.app.appconfig;
+
+import com.bot.app.MyWizardTelegramBot;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public MyWizardTelegramBot MySuperTelegramBot() {
+        DefaultBotOptions options = new DefaultBotOptions();
+        options.setProxyHost(proxyHost);
+        options.setProxyPort(proxyPort);
+        options.setProxyType(proxyType);
+
+        MyWizardTelegramBot mySuperTelegramBot = new MyWizardTelegramBot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+        return mySuperTelegramBot;
+    }
+}
